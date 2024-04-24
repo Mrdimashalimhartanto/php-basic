9bd9b121c103a4634369bb752360c19411f2343d
Dimas Halim Hartanto <dimashalimgit@gmail.com>
2024-04-24 14:48:31 +0700

    basic php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// var dump - untuk melihat isi variable 
+// print r - digunakan untuk debuging
+// echo - hasil dari php 
+
+
+// MATERI PENGULANGAN
+/**
+ * pengulangan
+ * for
+ * while
+ * do..while
+ * foreach : Pengulangan khusus array
+ */
+
+// pengulangan for
+// for ($i = 0; $i < 5; $i++) {
+//     echo " Dimas Halim Hartanto <br>";
+// }
+
+// pengulangan while 
+// $a = 0;
+// while ($a < 5) {
+//     echo "Hello Dimas <br>";
+//     $a++;
+// }
+
+// pengulangan do while
+do {
+    # code...
+} while ($a < 20);
